b3176641c6e9d8aa12085c1e947ebf76277cdbd6
chenwei <1015033746@qq.com>
2020-04-08 23:49:27 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,86 @@
+module.exports = {
+    title: '学习笔记',
+	base: 'learn_book',
+    description: '记录个人的学习笔记',
+    plugins: ['@vuepress/back-to-top'],
+    themeConfig: {
+        repo: 'learn/book',
+        repoLabel: '查看源码',
+        lastUpdated: '上次更新:',
+        smoothScroll: true,
+        algolia: {
+            applicationID: '8FPRMMKIX1',
+            apiKey: 'fd5b150f8d173cc4651d7e6f0c233c46',
+            adminApiKey: '25fa6207deecbaaad32aacca9e631fca',
+            indexName: 'truebt',
+            chunkSize: 5000
+        },
+        sidebarDepth: 2,
+        nav: [{
+                text: '前端',
+                items: [{
+                        text: 'HTML',
+                        link: '/guide/front/html/'
+                    },
+                    {
+                        text: 'CSS',
+                        link: '/guide/front/css/'
+                    },
+                    {
+                        text: 'JS',
+                        link: '/guide/front/js/'
+                    },
+                ]
+            },
+            {
+                text: 'Java',
+                items: [{
+                        text: 'Java基础',
+                        link: '/guide/back/java/base'
+                    },
+                    {
+                        text: 'Java',
+                        link: '/guide/back/java/base'
+                    },
+                ]
+            },
+            {
+                text: 'Go',
+                items: [{
+                        text: 'Go基础',
+                        link: '/guide/back/go/base'
+                    },
+                    {
+                        text: 'Java',
+                        link: '/guide/back/go/base'
+                    },
+                ]
+            },
+            {
+                text: 'Python',
+                items: [{
+                    text: 'Python基础',
+                    link: '/guide/back/python/base'
+                }]
+            },
+            {
+                text: 'Linux',
+                items: [{
+                    text: 'Linux基础',
+                    link: '/guide/devops/linux/base/'
+                }]
+            },
+        ],
+        sidebar: {
+            '/guide/devops/linux/base/': [
+                '',
+                '001_行业介绍',
+                '002_Linux简介',
+                '003_Linux安装'
+            ]
+
+
+
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,17 @@
+---
+home: true
+heroText: 全栈开发
+tagline:  Vue, Java, MySql, Jenkins, Docker, K8S
+actionText: 学习路线 →
+actionLink: /zh/guide/
+features:
+- title: 前端
+  details: 核心放在 Vue全家桶、ElementUI, ES6, Uni-App, 小程序
+- title: 后端
+  details: Java Web, 微服务, 工具集, 分布式解决方案
+- title: 运维
+  details: Jenkins, Docker, K8S, Linux
+- title: 数据库
+  details: Mysql 
+footer: MIT Licensed | Copyright © 2020-present TrueBt
+---
